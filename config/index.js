9228cae37feb248a8af2587d879173a8f1f8b944
allenou <jskindler@outlook.com>
2022-06-25 12:03:40 +0800

    chore: alias

--- config/index.js
+++ config/index.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const config = {
   projectName: 'app',
   date: '2022-6-24',
@@ -5,22 +7,26 @@ const config = {
   deviceRatio: {
     640: 2.34 / 2,
     750: 1,
-    828: 1.81 / 2
+    828: 1.81 / 2,
   },
   sourceRoot: 'src',
   outputRoot: 'dist',
-  plugins: ['taro-plugin-pinia'],
-  defineConstants: {
-  },
+  plugins: ['@tarojs/plugin-html', 'taro-plugin-pinia'],
+  defineConstants: {},
   copy: {
-    patterns: [
-    ],
-    options: {
-    }
+    patterns: [],
+    options: {},
   },
   framework: 'vue3',
+  sass: {
+    data: `@import "@nutui/nutui-taro/dist/styles/variables.scss";`,
+  },
+  alias: {
+    '@/components': path.resolve(__dirname, '..', 'src/components'),
+    '@/assets': path.resolve(__dirname, '..', 'src/assets'),
+  },
   mini: {
-    webpackChain (chain) {
+    webpackChain(chain) {
       chain.merge({
         module: {
           rule: {
@@ -29,35 +35,33 @@ const config = {
               include: [/pinia/],
               use: {
                 babelLoader: {
-                  loader: require.resolve('babel-loader')
-                }
-              }
-            }
-          }
-        }
+                  loader: require.resolve('babel-loader'),
+                },
+              },
+            },
+          },
+        },
       })
     },
     postcss: {
       pxtransform: {
         enable: true,
-        config: {
-
-        }
+        config: {},
       },
       url: {
         enable: true,
         config: {
-          limit: 1024 // 设定转换尺寸上限
-        }
+          limit: 1024, // 设定转换尺寸上限
+        },
       },
       cssModules: {
         enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
         config: {
           namingPattern: 'module', // 转换模式，取值为 global/module
-          generateScopedName: '[name]__[local]___[hash:base64:5]'
-        }
-      }
-    }
+          generateScopedName: '[name]__[local]___[hash:base64:5]',
+        },
+      },
+    },
   },
   h5: {
     publicPath: '/',
@@ -65,18 +69,17 @@ const config = {
     postcss: {
       autoprefixer: {
         enable: true,
-        config: {
-        }
+        config: {},
       },
       cssModules: {
         enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
         config: {
           namingPattern: 'module', // 转换模式，取值为 global/module
-          generateScopedName: '[name]__[local]___[hash:base64:5]'
-        }
-      }
-    }
-  }
+          generateScopedName: '[name]__[local]___[hash:base64:5]',
+        },
+      },
+    },
+  },
 }
 
 module.exports = function (merge) {
