49d737ba34cd4b17048d7f174c01ad5f5addc403
allenou <jskindler@outlook.com>
2022-06-25 12:22:19 +0800

    chore: 引入 nut ui

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { Button, Icon } from "@nutui/nutui-taro";
-
+// import NutUI from '@nutui/nutui-taro'
+// import '@nutui/nutui-taro/dist/style.css'
+import { Button, Cell, Icon } from '@nutui/nutui-taro'
+import '@nutui/nutui-taro/dist/styles/themes/default.scss';
 import './app.scss'
 
 const App = createApp({
-  onShow(options) {
-  },
+  onShow(options) {},
   // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
 })
 
-App.use(createPinia())
-App.use(Button)
+App.use(createPinia()).use(Button).use(Cell).use(Icon)
 export default App
